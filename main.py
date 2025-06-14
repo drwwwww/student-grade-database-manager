b2f26c36ed9f26962f7fd2003a7e6802a2f93bef
drwwwww <ledanreba@gmail.com>
2025-06-14 02:48:29 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import sqlite3
+from colorama import init, Fore, Style
+
+init()
+
+def main():
+    print(Fore.GREEN + Style.BRIGHT + "----- welcome to the school's grading system -----")
+    print()
+    print("To get started please tell us if you are trying to connect to a exisiting class or make a new one?" + Style.RESET_ALL)
+    
+
+main()
