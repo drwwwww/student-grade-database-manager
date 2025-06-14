a7d5425845441a7c38280dfd41303473159c699d
drwwwww <ledanreba@gmail.com>
2025-06-14 02:49:31 -0400

    test commit

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def main():
     print(Fore.GREEN + Style.BRIGHT + "----- welcome to the school's grading system -----")
     print()
     print("To get started please tell us if you are trying to connect to a exisiting class or make a new one?" + Style.RESET_ALL)
+    print()
     
 
 main()
