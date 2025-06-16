d0671af80a9663163879766f69902cf316772a7f
drwwwww <ledanreba@gmail.com>
2025-06-15 20:18:15 -0400

    Getting more started

--- main.py
+++ main.py
@@ -6,8 +6,17 @@ init()
 def main():
     print(Fore.GREEN + Style.BRIGHT + "----- welcome to the school's grading system -----")
     print()
-    print("To get started please tell us if you are trying to connect to a exisiting class or make a new one?" + Style.RESET_ALL)
-    print()
+    print("To get started please tell us if you are trying to connect to a exisiting class or make a new one?")
+    choice = input("Answer (Connect or New): ")
+
+    if choice == "connect" or "Connect" or "Conn" or "conn":
+        print()
     
+    if choice == "new" or "New":
+        base = input("What would you like to call the new database?: " + Style.RESET_ALL)
+        database = base + ".db"
+        conn = sqlite3.connect(database)
+        cursor = conn.cursor()
+
 
 main()
